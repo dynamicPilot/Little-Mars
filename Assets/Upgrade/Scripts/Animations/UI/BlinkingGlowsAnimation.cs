960d29748437a5913bd7738dc74cdbf4ccfa83ea
dynamicPilot <khudyakova.valya@gmail.com>
2023-06-24 22:33:13 +0600

    BlinkingGlowsAnimation. Background Button.

--- Assets/Upgrade/Scripts/Animations/UI/BlinkingGlowsAnimation.cs
+++ Assets/Upgrade/Scripts/Animations/UI/BlinkingGlowsAnimation.cs
@@ -1,21 +1,19 @@
-using DG.Tweening;
-using System;
+﻿using DG.Tweening;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace LittleMars.Animations.UI
 {
-
-    public class BlinkingGlowAnimation : TweenAnimation
+    public class BlinkingGlowsAnimation : TweenAnimation
     {
-        [SerializeField] private Image _indicator;
+        [SerializeField] Image[] _indicators;
         [SerializeField] private float _endFade;
         [SerializeField] private float _period;
 
         [Header("Animation ID")]
         [SerializeField] private bool _needSetId = true;
-        [SerializeField] string _id = "blinkingGlow";
+        [SerializeField] string _id = "blinkingGlow_";
 
         [Header("TimeScale Settings")]
         [SerializeField] bool _unscaledTime = false;
@@ -27,31 +25,33 @@ namespace LittleMars.Animations.UI
 
         public void StartAnimation()
         {
-            Blinking();
+            for (int i= 0; i<_indicators.Length;i++) Blinking(i);
         }
 
-        private void Blinking()
+        void Blinking(int index)
         {
             Sequence sequence = DOTween.Sequence();
 
             sequence
-                .Append(_indicator.DOFade(0f, _period))
-                .Append(_indicator.DOFade(_endFade, _period))
-                .Append(_indicator.DOFade(0f, _period))
+                .Append(_indicators[index].DOFade(0f, _period))
+                .Append(_indicators[index].DOFade(_endFade, _period))
+                .Append(_indicators[index].DOFade(0f, _period))
                 .SetLoops(-1);
 
             if (_unscaledTime) sequence.SetUpdate(_unscaledTime);
-            if (_needSetId) sequence.SetId(_id);
+            if (_needSetId) sequence.SetId(_id.Concat(index.ToString()));
         }
 
         public void Play()
         {
-            if (_needSetId) DOTween.Play(_id);
+            for (int i = 0; i < _indicators.Length; i++)
+                if (_needSetId) DOTween.Play(_id.Concat(i.ToString()));
         }
 
         public void Pause()
         {
-            if (_needSetId) DOTween.Pause(_id);
+            for (int i = 0; i < _indicators.Length; i++)
+                if (_needSetId) DOTween.Pause(_id.Concat(i.ToString()));
         }
 
         public void OnDestroy()
---
+++ Assets/Upgrade/Scripts/UI/WindowClickBackground.cs
@@ -0,0 +1,21 @@
+﻿using LittleMars.LevelMenus;
+using LittleMars.UI.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace LittleMars.UI
+{
+    public class WindowClickBackground : MonoBehaviour, IPointerClickHandler
+    {
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            
+        }
+    }
+}
